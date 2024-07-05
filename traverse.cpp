f41259eb87f972329c8950ba0075768e9e20179c
aiman-munawar <aimanmunawar6@gmail.com>
2024-07-05 22:57:26 +0500

    change

---
+++ traverse.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=5,i=0;
+    int data[n]={1,3,5,6,7};
+      while(i<n){
+        cout<<data[i]<<"  ";
+        i++;
+        }
+      
+}
